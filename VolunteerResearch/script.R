49aaf50cecf037aeba9c4950e0d903fde1e7814d
Zach Cartnick <zachcartnick@gmail.com>
2019-01-15 12:21:40 -0500

    Progress...
    Started trying to compare models

--- VolunteerResearch/script.R
+++ VolunteerResearch/script.R
@@ -1,12 +1,24 @@
+#install.packages("tidyverse")
+#install.packages("devtools")
+#devtools::install_github("dtkaplan/statisticalModeling")
 library(ggplot2)
 library(statisticalModeling)
 library(rpart)
 
-dataset <- read_csv("dataset.csv")
-model <- lm(volunteer ~ friends + leadership + resume, data = dataset)
+dataset <- read.csv("dataset.csv")
+dataset$bogus <- rnorm(nrow(dataset)) > 0
+dataset$training_cases <- rnorm(nrow(dataset)) > 0
 
-sample <- data.frame(friends = 1, leadership = 1, resume = 0)
+model <- lm(volunteer ~ school_type + extra, data = subset(dataset, training_cases))
+model_output <- evaluate_model(model, newdata = subset(dataset, !training_cases))
 
-r1 <- predict(sample, model)
+aug_model <- lm(volunteer ~ school_type + extra + bogus, data = subset(dataset, training_cases))
+aug_model_output <- evaluate_model(aug_model, newdata = subset(dataset, !training_cases))
 
+model_diff <- with(dataset, volunteer - model_output)
+aug_model_diff <- with(dataset, volunteer - aug_model_output)
 
+mean(model_diff ^ 2)
+mean(aug_model_diff ^ 2)
+
+fmodel(model, ~ school_type + extra)
