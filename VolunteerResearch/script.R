9a101306b268a8ce22f98c3a11e40448ab67114f
Zach Cartnick <zachcartnick@gmail.com>
2019-01-16 12:19:56 -0500

    Progress...

--- VolunteerResearch/script.R
+++ VolunteerResearch/script.R
@@ -1,18 +1,23 @@
 #install.packages("tidyverse")
 #install.packages("devtools")
 #devtools::install_github("dtkaplan/statisticalModeling")
+library(tidyverse)
 library(ggplot2)
 library(statisticalModeling)
 library(rpart)
 
 dataset <- read.csv("dataset.csv")
+
+
+as_tibble(dataset)
+
 dataset$bogus <- rnorm(nrow(dataset)) > 0
 dataset$training_cases <- rnorm(nrow(dataset)) > 0
 
 model <- lm(volunteer ~ school_type + extra, data = subset(dataset, training_cases))
 model_output <- evaluate_model(model, newdata = subset(dataset, !training_cases))
 
-aug_model <- lm(volunteer ~ school_type + extra + bogus, data = subset(dataset, training_cases))
+aug_model <- loess(volunteer ~ school_type + extra, data = subset(dataset, training_cases))
 aug_model_output <- evaluate_model(aug_model, newdata = subset(dataset, !training_cases))
 
 model_diff <- with(dataset, volunteer - model_output)
